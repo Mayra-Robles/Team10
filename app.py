6ffc2fe778bd9a472b1dd61cdb361398027c6666
radiolocked <dilancl808@gmail.com>
2025-03-21 16:10:08 -0600

    app.py updated ot properly display demo project on frontend

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from ProjectManager import ProjectManager
 
 app = Flask(__name__)
+CORS(app) #enable CORS
 pm = ProjectManager()
 pm.load_from_file()
 
